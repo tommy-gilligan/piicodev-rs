10400dbe5bca47144d44b17fb8b79f560adaa33e
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-04-27 19:06:08 +1000

    Move ci subcommand to single match branch

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -56,52 +56,52 @@ fn main() {
         .subcommand_required(true)
         .subcommand(clap::command!("ci"));
     let matches = cmd.get_matches();
-    let _matches = match matches.subcommand() {
-        Some(("ci", matches)) => matches,
+    match matches.subcommand() {
+        Some(("ci", _)) => {
+            let metadata = MetadataCommand::new()
+                .manifest_path("./Cargo.toml")
+                .features(CargoOpt::AllFeatures)
+                .exec()
+                .unwrap();
+
+            // exclude xtask
+            let workspace_members = metadata
+                .workspace_members
+                .into_iter()
+                .filter(|package_id| package_id.repr.starts_with('p'));
+
+            let mut links: HashMap<String, Vec<camino::Utf8PathBuf>> = HashMap::new();
+            for member_package_id in workspace_members {
+                let package = find_package(&metadata.packages, member_package_id).unwrap();
+                let readme_path = package.readme().unwrap();
+                let mut file = File::open(readme_path.clone()).unwrap();
+                let mut contents = String::new();
+                file.read_to_string(&mut contents).unwrap();
+                let mdast = markdown::to_mdast(&contents, &markdown::ParseOptions::gfm()).unwrap();
+
+                for required_link in REQUIRED_LINKS {
+                    if find_definition(&mdast, required_link).is_none() {
+                        panic!("{} definition not found in {}", required_link, readme_path);
+                    }
+                }
+                for unique_link in UNIQUE_LINKS {
+                    if let Some(definition) = find_definition(&mdast, unique_link) {
+                        links
+                            .entry(definition.url.clone())
+                            .and_modify(|v| v.push(readme_path.clone()))
+                            .or_insert(vec![readme_path.clone()]);
+                    }
+                }
+                for (url, readme_paths) in links.iter() {
+                    if readme_paths.len() > 1 {
+                        panic!("duplicate url {} in {:?}", url, readme_paths);
+                    }
+                }
+                if find_main_heading(&mdast).is_none() {
+                    panic!("no heading in {:?}", readme_path);
+                }
+            }
+        }
         _ => unreachable!("clap should ensure we don't get here"),
     };
-
-    let metadata = MetadataCommand::new()
-        .manifest_path("./Cargo.toml")
-        .features(CargoOpt::AllFeatures)
-        .exec()
-        .unwrap();
-
-    // exclude xtask
-    let workspace_members = metadata
-        .workspace_members
-        .into_iter()
-        .filter(|package_id| package_id.repr.starts_with('p'));
-
-    let mut links: HashMap<String, Vec<camino::Utf8PathBuf>> = HashMap::new();
-    for member_package_id in workspace_members {
-        let package = find_package(&metadata.packages, member_package_id).unwrap();
-        let readme_path = package.readme().unwrap();
-        let mut file = File::open(readme_path.clone()).unwrap();
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        let mdast = markdown::to_mdast(&contents, &markdown::ParseOptions::gfm()).unwrap();
-
-        for required_link in REQUIRED_LINKS {
-            if find_definition(&mdast, required_link).is_none() {
-                panic!("{} definition not found in {}", required_link, readme_path);
-            }
-        }
-        for unique_link in UNIQUE_LINKS {
-            if let Some(definition) = find_definition(&mdast, unique_link) {
-                links
-                    .entry(definition.url.clone())
-                    .and_modify(|v| v.push(readme_path.clone()))
-                    .or_insert(vec![readme_path.clone()]);
-            }
-        }
-        for (url, readme_paths) in links.iter() {
-            if readme_paths.len() > 1 {
-                panic!("duplicate url {} in {:?}", url, readme_paths);
-            }
-        }
-        if find_main_heading(&mdast).is_none() {
-            panic!("no heading in {:?}", readme_path);
-        }
-    }
 }
