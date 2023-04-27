de82f49a04580623ee01197858e85426d3f96f94
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-04-27 16:30:02 +1000

    Check required links for duplicates

--- xtask/src/bin/ci.rs
+++ xtask/src/bin/ci.rs
@@ -1,4 +1,5 @@
 use markdown::mdast::Definition;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
 use std::string::String;
@@ -13,14 +14,19 @@ const REQUIRED_LINKS: [&str; 3] = [
 
 fn find_definition<'a>(
     node: &'a markdown::mdast::Node,
-    needle_label: &'a str,
-) -> Option<&'a markdown::mdast::Node> {
-    node.children().unwrap().iter().find(|node| match *node {
-        markdown::mdast::Node::Definition(Definition {
-            label: Some(label), ..
-        }) => label == needle_label,
-        _ => false,
-    })
+    needle_label: &str,
+) -> Option<&'a Definition> {
+    node.children()
+        .unwrap()
+        .iter()
+        .find_map(|node| match *node {
+            markdown::mdast::Node::Definition(ref definition)
+                if definition.label.as_deref() == Some(needle_label) =>
+            {
+                Some(definition)
+            }
+            _ => None,
+        })
 }
 
 fn find_package(packages: &[Package], id: PackageId) -> Option<&Package> {
@@ -40,6 +46,7 @@ fn main() {
         .into_iter()
         .filter(|package_id| package_id.repr.starts_with('p'));
 
+    let mut links: HashSet<String> = HashSet::new();
     for member_package_id in workspace_members {
         let package = find_package(&metadata.packages, member_package_id).unwrap();
         let readme_path = package.readme().unwrap();
@@ -49,7 +56,16 @@ fn main() {
         let mdast = markdown::to_mdast(&contents, &markdown::ParseOptions::gfm()).unwrap();
 
         for required_link in REQUIRED_LINKS {
-            if find_definition(&mdast, required_link).is_none() {
+            if let Some(definition) = find_definition(&mdast, required_link) {
+                if links.contains(&definition.url) {
+                    panic!(
+                        "duplicate url {} for link {} in {}",
+                        definition.url, required_link, readme_path
+                    );
+                } else {
+                    links.insert(definition.url.clone());
+                }
+            } else {
                 panic!("{} definition not found in {}", required_link, readme_path);
             }
         }
