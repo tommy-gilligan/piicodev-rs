945e2788f7a795fa0ef32a5be6806c098a6d7732
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-05-11 00:34:52 +1000

    add p15 magnetometer

---
+++ p15/README.md
@@ -0,0 +1,10 @@
+[Official Hardware Repository]: https://github.com/CoreElectronics/CE-PiicoDev-Magnetometer-QMC6310
+[Official MicroPython Repository]: https://github.com/CoreElectronics/CE-PiicoDev-QMC6310-MicroPython-Module
+[Official Product Site]: https://piico.dev/p15
+[Datasheet]: https://datasheet.lcsc.com/lcsc/2007101835_QST-QMC6310U_C669299.pdf
+# Unofficial Rust Driver for PiicoDev Magnetometer
+## External Links
+- [Official Hardware Repository]
+- [Official MicroPython Repository]
+- [Official Product Site]
+- [Datasheet]
---
+++ p15/src/lib.rs
@@ -0,0 +1,244 @@
+#![doc = include_str!("../README.md")]
+#![no_std]
+#![feature(lint_reasons)]
+
+use embedded_hal::i2c::I2c;
+
+pub struct P15<I2C> {
+    i2c: I2C,
+    address: u8,
+}
+
+const ADDRESS_CONTROL1: u8 = 0x0A;
+const CONTROL1_VALUE: u8 = 0b11001101;
+const ADDRESS_CONTROL2: u8 = 0x0B;
+const CONTROL2_VALUE: u8 = 0x00;
+const ADDRESS_SIGN: u8 = 0x29;
+const SIGN_VALUE: u8 = 0b00000110;
+const ADDRESS_STATUS: u8 = 0x09;
+const ADDRESS_XOUT: u8 = 0x01;
+const ADDRESS_YOUT: u8 = 0x03;
+const ADDRESS_ZOUT: u8 = 0x05;
+
+use libm::{atan2, sqrt};
+use measurements::Angle;
+
+impl<I2C: I2c> P15<I2C> {
+    pub fn new(i2c: I2C, address: u8) -> Result<Self, I2C::Error> {
+        let mut res = Self { i2c, address };
+        res.set_sign()?;
+        res.set_range()?;
+        res.set_control_register()?;
+        Ok(res)
+    }
+
+    fn set_control_register(&mut self) -> Result<(), I2C::Error> {
+        self.i2c
+            .write(self.address, &[ADDRESS_CONTROL1, CONTROL1_VALUE])?;
+
+        Ok(())
+    }
+
+    fn set_range(&mut self) -> Result<(), I2C::Error> {
+        self.i2c
+            .write(self.address, &[ADDRESS_CONTROL2, CONTROL2_VALUE])?;
+
+        Ok(())
+    }
+
+    fn set_sign(&mut self) -> Result<(), I2C::Error> {
+        self.i2c.write(self.address, &[ADDRESS_SIGN, SIGN_VALUE])?;
+
+        Ok(())
+    }
+
+    pub fn data_ready(&mut self) -> Result<bool, I2C::Error> {
+        let mut data: [u8; 1] = [0];
+        self.i2c
+            .write_read(self.address, &[ADDRESS_STATUS], &mut data)?;
+        if (data[0] & 0b00000011) == 0x01 {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    pub fn read(&mut self) -> Result<(f64, f64, f64), I2C::Error> {
+        let mut data_x: [u8; 2] = [0, 0];
+        self.i2c
+            .write_read(self.address, &[ADDRESS_XOUT], &mut data_x)?;
+        let mut data_y: [u8; 2] = [0, 0];
+        self.i2c
+            .write_read(self.address, &[ADDRESS_YOUT], &mut data_y)?;
+        let mut data_z: [u8; 2] = [0, 0];
+        self.i2c
+            .write_read(self.address, &[ADDRESS_ZOUT], &mut data_z)?;
+        Ok((
+            i16::from_le_bytes(data_x) as f64 * 0.1f64,
+            i16::from_le_bytes(data_y) as f64 * 0.1f64,
+            i16::from_le_bytes(data_z) as f64 * 0.1f64,
+        ))
+    }
+
+    pub fn read_angle(&mut self) -> Result<Angle, I2C::Error> {
+        let (x, y, _) = self.read()?;
+        Ok(Angle::from_radians(atan2(x, -y)))
+    }
+
+    pub fn read_magnitude(&mut self) -> Result<f64, I2C::Error> {
+        let (x, y, z) = self.read()?;
+        Ok(sqrt(x * x + y * y + z * z))
+    }
+}
+
+#[cfg(all(test, not(all(target_arch = "arm", target_os = "none"))))]
+#[macro_use]
+extern crate std;
+
+#[cfg(all(test, not(all(target_arch = "arm", target_os = "none"))))]
+mod test {
+    extern crate embedded_hal;
+    extern crate embedded_hal_mock;
+    use embedded_hal_mock::i2c::{Mock as I2cMock, Transaction as I2cTransaction};
+    use measurements::Angle;
+
+    use crate::P15;
+
+    #[test]
+    pub fn set_control_register() {
+        let expectations = [I2cTransaction::write(0x1C, vec![0x0A, 0b11001101])];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+
+        assert_eq!(p15.set_control_register(), Ok(()));
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn set_sign() {
+        let expectations = [I2cTransaction::write(0x1C, vec![0x29, 0x06])];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+
+        assert_eq!(p15.set_sign(), Ok(()));
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn set_range() {
+        let expectations = [I2cTransaction::write(0x1C, vec![0x0B, 0x00])];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+
+        assert_eq!(p15.set_range(), Ok(()));
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn new() {
+        let expectations = [
+            I2cTransaction::write(0x1C, vec![0x29, 0x06]),
+            I2cTransaction::write(0x1C, vec![0x0B, 0x00]),
+            I2cTransaction::write(0x1C, vec![0x0A, 0b11001101]),
+        ];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        P15::new(i2c, 0x1C).unwrap();
+
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn data_ready_true() {
+        let expectations = [I2cTransaction::write_read(0x1C, vec![0x09], vec![0x01])];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+        assert_eq!(p15.data_ready(), Ok(true));
+
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn data_ready_false() {
+        let expectations = [I2cTransaction::write_read(0x1C, vec![0x09], vec![0x00])];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+        assert_eq!(p15.data_ready(), Ok(false));
+
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn data_ready_overflow() {
+        let expectations = [I2cTransaction::write_read(0x1C, vec![0x09], vec![0x03])];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+        assert_eq!(p15.data_ready(), Ok(false));
+
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn read() {
+        let expectations = [
+            I2cTransaction::write_read(0x1C, vec![0x01], vec![0x10, 0x01]),
+            I2cTransaction::write_read(0x1C, vec![0x03], vec![0xDF, 0xFD]),
+            I2cTransaction::write_read(0x1C, vec![0x05], vec![0x30, 0x03]),
+        ];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+        assert_eq!(
+            p15.read(),
+            Ok((27.200000000000003f64, -54.5f64, 81.60000000000001f64))
+        );
+
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn read_angle() {
+        let expectations = [
+            I2cTransaction::write_read(0x1C, vec![0x01], vec![0x10, 0x01]),
+            I2cTransaction::write_read(0x1C, vec![0x03], vec![0xDF, 0xFD]),
+            I2cTransaction::write_read(0x1C, vec![0x05], vec![0x30, 0x03]),
+        ];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+        assert_eq!(p15.read_angle(), Ok(Angle::from_degrees(26.52298379879782)));
+
+        i2c_clone.done();
+    }
+
+    #[test]
+    pub fn read_magnitude() {
+        let expectations = [
+            I2cTransaction::write_read(0x1C, vec![0x01], vec![0x10, 0x01]),
+            I2cTransaction::write_read(0x1C, vec![0x03], vec![0xDF, 0xFD]),
+            I2cTransaction::write_read(0x1C, vec![0x05], vec![0x30, 0x03]),
+        ];
+        let i2c = I2cMock::new(&expectations);
+        let mut i2c_clone = i2c.clone();
+
+        let mut p15 = P15 { i2c, address: 0x1C };
+        assert_eq!(p15.read_magnitude(), Ok(101.82656824228145));
+
+        i2c_clone.done();
+    }
+}
