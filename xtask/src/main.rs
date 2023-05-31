6443f92a87fae2f4ce127c566bb841ad85501bae
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-05-31 21:08:43 +1000

    Factor out CI to lower complexity

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -119,6 +119,87 @@ fn check_links() {
     }
 }
 
+fn ci() {
+    let metadata = MetadataCommand::new()
+        .manifest_path("./Cargo.toml")
+        .features(CargoOpt::AllFeatures)
+        .exec()
+        .unwrap();
+
+    // exclude xtask
+    let mut workspace_members = metadata
+        .workspace_members
+        .into_iter()
+        .filter(|package_id| package_id.repr.starts_with('p'))
+        .collect::<Vec<PackageId>>();
+    workspace_members.sort_by_key(|k| {
+        return (*(k.repr))
+            .strip_prefix('p')
+            .unwrap()
+            .split(' ')
+            .next()
+            .unwrap()
+            .parse::<u8>()
+            .unwrap();
+    });
+
+    let mut links: HashMap<String, Vec<camino::Utf8PathBuf>> = HashMap::new();
+    let mut all_keywords: HashMap<String, u8> = HashMap::new();
+    for member_package_id in workspace_members {
+        let package = find_package(&metadata.packages, &member_package_id).unwrap();
+
+        let keywords = &mut package.keywords.clone();
+        keywords.sort();
+        all_keywords
+            .entry(keywords.join(","))
+            .and_modify(|v| *v += 1)
+            .or_insert_with(|| 1);
+
+        let readme_path = package.readme().unwrap();
+        let mut file = File::open(readme_path.clone()).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let mdast = markdown::to_mdast(&contents, &markdown::ParseOptions::gfm()).unwrap();
+
+        for required_link in REQUIRED_LINKS {
+            assert!(
+                find_definition(&mdast, required_link).is_some(),
+                "{required_link} definition not found in {readme_path}"
+            );
+        }
+        for unique_link in UNIQUE_LINKS {
+            if let Some(definition) = find_definition(&mdast, unique_link) {
+                links
+                    .entry(definition.url.clone())
+                    .and_modify(|v| v.push(readme_path.clone()))
+                    .or_insert_with(|| vec![readme_path.clone()]);
+            }
+        }
+        assert!(
+            find_main_heading(&mdast).is_some(),
+            "no heading in {readme_path:?}"
+        );
+        let title: String = text(find_main_heading(&mdast).unwrap()).unwrap();
+        let title_without_prefix: &str = title.strip_prefix(TITLE_PREFIX).unwrap();
+        let mut components = package.manifest_path.components().rev();
+        components.next();
+        println!(
+            "- [{}](./{}/)",
+            title_without_prefix,
+            components.next().unwrap()
+        );
+    }
+    for (url, readme_paths) in &links {
+        assert!(
+            readme_paths.len() <= 1,
+            "duplicate url {url} in {readme_paths:?}"
+        );
+    }
+    for (keywords, len) in &all_keywords {
+        assert!(*len <= 1, "duplicate keywords {keywords}");
+    }
+}
+
 fn main() {
     let cmd = clap::Command::new("xtask")
         .bin_name("xtask")
@@ -128,86 +209,7 @@ fn main() {
     let matches = cmd.get_matches();
     match matches.subcommand() {
         Some(("check-links", _)) => check_links(),
-        Some(("ci", _)) => {
-            let metadata = MetadataCommand::new()
-                .manifest_path("./Cargo.toml")
-                .features(CargoOpt::AllFeatures)
-                .exec()
-                .unwrap();
-
-            // exclude xtask
-            let mut workspace_members = metadata
-                .workspace_members
-                .into_iter()
-                .filter(|package_id| package_id.repr.starts_with('p'))
-                .collect::<Vec<PackageId>>();
-            workspace_members.sort_by_key(|k| {
-                return (*(k.repr))
-                    .strip_prefix('p')
-                    .unwrap()
-                    .split(' ')
-                    .next()
-                    .unwrap()
-                    .parse::<u8>()
-                    .unwrap();
-            });
-
-            let mut links: HashMap<String, Vec<camino::Utf8PathBuf>> = HashMap::new();
-            let mut all_keywords: HashMap<String, u8> = HashMap::new();
-            for member_package_id in workspace_members {
-                let package = find_package(&metadata.packages, &member_package_id).unwrap();
-
-                let keywords = &mut package.keywords.clone();
-                keywords.sort();
-                all_keywords
-                    .entry(keywords.join(","))
-                    .and_modify(|v| *v += 1)
-                    .or_insert_with(|| 1);
-
-                let readme_path = package.readme().unwrap();
-                let mut file = File::open(readme_path.clone()).unwrap();
-                let mut contents = String::new();
-                file.read_to_string(&mut contents).unwrap();
-                let mdast = markdown::to_mdast(&contents, &markdown::ParseOptions::gfm()).unwrap();
-
-                for required_link in REQUIRED_LINKS {
-                    assert!(
-                        find_definition(&mdast, required_link).is_some(),
-                        "{required_link} definition not found in {readme_path}"
-                    );
-                }
-                for unique_link in UNIQUE_LINKS {
-                    if let Some(definition) = find_definition(&mdast, unique_link) {
-                        links
-                            .entry(definition.url.clone())
-                            .and_modify(|v| v.push(readme_path.clone()))
-                            .or_insert_with(|| vec![readme_path.clone()]);
-                    }
-                }
-                assert!(
-                    find_main_heading(&mdast).is_some(),
-                    "no heading in {readme_path:?}"
-                );
-                let title: String = text(find_main_heading(&mdast).unwrap()).unwrap();
-                let title_without_prefix: &str = title.strip_prefix(TITLE_PREFIX).unwrap();
-                let mut components = package.manifest_path.components().rev();
-                components.next();
-                println!(
-                    "- [{}](./{}/)",
-                    title_without_prefix,
-                    components.next().unwrap()
-                );
-            }
-            for (url, readme_paths) in &links {
-                assert!(
-                    readme_paths.len() <= 1,
-                    "duplicate url {url} in {readme_paths:?}"
-                );
-            }
-            for (keywords, len) in &all_keywords {
-                assert!(*len <= 1, "duplicate keywords {keywords}");
-            }
-        }
+        Some(("ci", _)) => ci(),
         _ => unreachable!("clap should ensure we don't get here"),
     };
 }
