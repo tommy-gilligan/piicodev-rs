f35ae84da74951161bf6b989681cf6c397e35ad0
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-09-27 14:09:32 +1000

    Disable other drivers until I've tested them properly

--- src/lib.rs
+++ src/lib.rs
@@ -84,22 +84,22 @@ pub trait DriverUsingDelay<I2C: I2c, DELAY: DelayUs, T> {
 }
 
 pub mod p1;
-pub mod p10;
-// pub mod p11;
-pub mod p12;
-pub mod p13;
-pub mod p14;
-// pub mod p15;
-pub mod p16;
-pub mod p18;
-pub mod p19;
-// pub mod p2;
-pub mod p21;
-pub mod p22;
-pub mod p23;
-pub mod p26;
-pub mod p27;
-pub mod p29;
 pub mod p3;
 pub mod p30;
 pub mod p7;
+// pub mod p10;
+// pub mod p11;
+// pub mod p12;
+// pub mod p13;
+// pub mod p14;
+// pub mod p15;
+// pub mod p16;
+// pub mod p18;
+// pub mod p19;
+// pub mod p2;
+// pub mod p21;
+// pub mod p22;
+// pub mod p23;
+// pub mod p26;
+// pub mod p27;
+// pub mod p29;
